9d677090814a9be47bdf4b1752724e37ce8a3dc6
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-13 18:49:09 +0530

    end to end testing

--- frontend/src/components/Note.test.jsx
+++ frontend/src/components/Note.test.jsx
@@ -1,84 +1,21 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
-import { expect } from 'vitest'
 
-// // test 1
-// test('renders content', () => {
-//   const note = {
-//     content: 'Component testing is done with react-testing-library',
-//     important: true
-//   }
-
-//   render(<Note note={note} />)
-
-//   const element = screen.getByText('Component testing is done with react-testing-library')
-//   expect(element).toBeDefined()
-// })
-
-//test 2
-test('shows right button label when note is not important' , () => {
+test('clicking the button calls event handler once', async () => {
   const note = {
-    content: 'Learning test is fun',
-    important: false
-  }
-  render(<Note note={note}/>)
-
-  const button = screen.getByText('make important')
-  expect(button).toBeDefined()
-})
-
-//test 3
-test('new test for me button', () => {
-  const note = {
-    content: 'hello',
+    content: 'Component testiing is done with react-testing-library',
     important: true
   }
-  render(<Note note={note} />)
+  const mockHandler = vi.fn()
 
-  const button = screen.getByText('make not important')
-  expect(button).toBeDefined()
-})
-
-//test 4
-test('does not render this', () => {
-  const note = {
-    content: 'This is a reminder',
-    important: true
-  }
-
-  render(<Note note={note}/>)
-
-  const element = screen.queryByText('do not want this thing to be renderd')
-  expect(element).toBeNull()
-})
-
-//test 5
-test('renders content', () => {
-  const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
-  }
-
-  const { container } = render(<Note note={note}/>)
-
-  const div = container.querySelector('.note')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
   )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
-
-
-//test 6
-test('Does not render this', () => {
-  const note = {
-    content: 'This is a reminder',
-    important: true
-  }
-
-  render(<Note note={note}/>)
-  screen.debug()
-
-  const element = screen.queryByText('do not want this thing to be renderd')
-  expect(element).toBeNull()
-})
-
---
+++ frontend/tests/note_app.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+test('front page can be opened', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+
+  const locator = page.getByText('Notes')
+  await expect(locator).toBeVisible()
+  await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+})
