3ea921e7044880ab297a6fbca66688cac46bad09
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-13 16:11:04 +0530

    5.14: Blog List Tests, step 2

--- bloglist-frontend-main/src/components/Blog.test.jsx
+++ bloglist-frontend-main/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but does not render URL or likes by dfault', () => {
@@ -23,4 +24,31 @@ test('renders title and author, but does not render URL or likes by dfault', () 
 
   const likesElement = screen.queryByText('likes : 15', { exact: false })
   expect(likesElement).toBeNull()
+})
+
+// 5.14: Blog List Tests, step 2
+test('after clicking the view button, URL and likes are displayed', async () => {
+  const blog = {
+    title: 'Testing the react components',
+    author: 'Yuvraj Singh',
+    url: 'www.fullstackopen.com/',
+    likes: 10 ,
+    user: {
+      username: 'Tester',
+      name: 'Test User'
+    }
+  }
+
+  render(<Blog blog={blog}/>)
+
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const urlElement = screen.getByText('www.fullstackopen.com/')
+  expect(urlElement).toBeDefined()
+
+  const likesElement = screen.getByText('likes 10', { exact: false })
+  expect(likesElement).toBeDefined()
 })
