20e662e29c705775c4ac51c4071f210e1a384dd4
Yuvraj Singh <yuvrajsteve@email.com>
2026-07-14 16:50:21 +0530

    5.18: Blog List End To End Testing, step 2

--- bloglist-frontend-main/tests/blog_app.spec.js
+++ bloglist-frontend-main/tests/blog_app.spec.js
@@ -1,20 +1,44 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Blog app', () => {
-  test.beforeEach(async ({ page }) => {
-
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Mandeep Singh',
+        username: 'mandeep',
+        password: 'password123'
+      }
+    })
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
-
     const usernameInput = page.getByLabel('username')
     const passwordInput = page.getByLabel('password')
-
     const loginButton = page.getByRole('button', { name: 'login' })
 
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
     await expect(loginButton).toBeVisible()
+  })
+
+  test.describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('mandeep')
+      await page.getByLabel('password').fill('password123')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Mandeep Singh')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('mandeep')
+      await page.getByLabel('password').fill('wrongpassword')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong', { exact: false })).toBeVisible()
+      await expect(page.getByText('Mandeep Singh')).not.toBeVisible()
+    })
   })
 })
